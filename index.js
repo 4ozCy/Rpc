902816f606bb8f35b738e560f034c219067d59dd
Wiston <157526994+Zyneeee@users.noreply.github.com>
2024-03-10 02:43:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ client.on('ready', async () => {
         .setType('PLAYING')
         .setURL('https://zcyy-website.vercel.app')
         .setState('invite My Bot')
-        .setName('Mario Is Red, Sonic Is Blue, Will You Be My Player 2.')
-        .setDetails('Go Visit My Website')
+        .setName('HBD To Me.')
+        .setDetails('Join My Bot Support Server')
         .setAssetsLargeImage('https://media.discordapp.net/attachments/1187377061959045181/1215063569578721333/22714827862d17e1a1a78bd344bfc5fc.jpg?ex=65fb6321&is=65e8ee21&hm=9c8262b8d6178831f8ad6cfe1dab9bb0efdd0cf9e1a092c470d354fb798778b8&')
-        .addButton('visit my website', 'https://zcyy-website.vercel.app')
+        .addButton('Support Server', 'https://discord.com/invite/SvF8YyQtgw')
         .addButton('Invite My Bot', 'https://discord.com/oauth2/authorize?client_id=1211738857192816681&permissions=68608&scope=bot+applications.commands');
 
     client.user.setActivity(r);
