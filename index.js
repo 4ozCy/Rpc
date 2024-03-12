54a3892f2c447fc83f91f422b68a8cc1197e8f0a
Wiston <157526994+Zyneeee@users.noreply.github.com>
2024-03-13 03:00:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.on('ready', async () => {
         .setType('PLAYING')
         .setURL('https://zcyy-website.vercel.app')
         .setState('invite My Bot')
-        .setName('HBD To Me.')
+        .setName('Are you a time traveller coz I can see u in my future.')
         .setDetails('Join My Bot Support Server')
         .setAssetsLargeImage('https://media.discordapp.net/attachments/1187377061959045181/1215063569578721333/22714827862d17e1a1a78bd344bfc5fc.jpg?ex=65fb6321&is=65e8ee21&hm=9c8262b8d6178831f8ad6cfe1dab9bb0efdd0cf9e1a092c470d354fb798778b8&')
         .addButton('Support Server', 'https://discord.com/invite/SvF8YyQtgw')
