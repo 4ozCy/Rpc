60f2e1227d60082e35442ef5a9bed8e49157f88a
Wiston <157526994+Zyneeee@users.noreply.github.com>
2024-05-24 12:05:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,13 +13,14 @@ client.on('ready', async () => {
 
     const r = new Discord.RichPresence()
         .setApplicationId('1107744228773220473')
-        .setType('LISTENING')
+        .setType('PLAYING')
         .setURL('https://zcyy-website.vercel.app')
-        .setState('Join My Community Server')
-        .setName('Bruno Mars')
-        .setDetails('Bruno Mars is the best 🎶')
+        .setState('Why is nothing is nothing and nothing is nothing')
+        .setName('Nothing')
+        .setDetails('Rose Are Red, Violets Are Blue, Im Asian')
         .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1187377061959045181/1224362471670812722/62f9e83690075ebc5c0d4c46ce6bf04e.jpg?ex=661d3768&is=660ac268&hm=646598b1b9bf1fd50d3362506dff1667ce90db8b88eec625c4b804f77cd5c0b3&')
-        .addButton('My Community Server', 'https://discord.com/invite/WUCm5Hkazq');
+        .addButton('My Community Server', 'https://discord.com/invite/H6Gv3ftYFU')
+        .addButton('Invite my bot', '');
 
     client.user.setActivity(r);
     client.user.setPresence({ status: 'dnd' });
