97903220ba6a8fb099ad4f00f6bde2699dc5486c
Zcy <157526994+Zyneeee@users.noreply.github.com>
2024-06-30 22:42:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.on('ready', async () => {
         .setURL('https://zcyy-website.vercel.app')
         .setState('pls join my community server')
         .setName('Nothing')
-        .setAssetsLargeImage('https://discords.com/_next/image?url=https%3A%2F%2Fcdn.discordapp.com%2Femojis%2F819233017243107358.png%3Fv%3D1&w=128&q=75')
+        .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1233503643500675252/1256997956637425674/c7ce50ce-fbbe-4589-a0e4-0c47a8d982ce.png?ex=6682ce12&is=66817c92&hm=76379a7f03b06623c8f9e7cf60f3f42d38a273c3bfffc7d22b917a7f849b60b8&')
         .addButton('my community server', 'https://discord.com/invite/H6Gv3ftYFU')
 
     client.user.setActivity(r);
