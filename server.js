06ed524c9b62aa48c8f29b9f0133ea6f7d9084dd
Zcy <157526994+Zyneeee@users.noreply.github.com>
2024-07-02 12:01:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const server = express();
  
 server.all('/', (req, res) => {
-  res.send(`AXO CODER OP`)
+  res.send(`made by: @nozcy.`)
 })
  
 function keepAlive() {
-  server.listen(3000, () => { console.log("Server is Ready!!" + Date.now()) });
+  server.listen(3000, () => { console.log("Server is Ready made by: @nozcy."+ Date.now()) });
 }
  
 module.exports = keepAlive;
