478e49a108f7c2f312707c538e3ef96bdd057a00
Zcy <157526994+Zyneeee@users.noreply.github.com>
2024-07-02 12:33:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ client.on('ready', async () => {
         .setApplicationId('1107744228773220473')
         .setType('PLAYING')
         .setURL('https://zcyy-website.vercel.app')
-        .setState('pls join my community server')
+        .setState('Click The Button below to join my server')
         .setName('Nothing')
-        .setDetails('click the button below to join my server')
+        .setDetails('pls join My Community Server')
         .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1233503643500675252/1256997956637425674/c7ce50ce-fbbe-4589-a0e4-0c47a8d982ce.png?ex=6682ce12&is=66817c92&hm=76379a7f03b06623c8f9e7cf60f3f42d38a273c3bfffc7d22b917a7f849b60b8&')
         .addButton('click here', 'https://discord.com/invite/tqnDqzmbxE')
 
