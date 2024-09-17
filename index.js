4260a5adf346ac6fbfcd3e1069123e8e34ab18b3
Zcy. <157526994+4ozCy@users.noreply.github.com>
2024-09-17 20:45:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 const Discord = require('discord.js-selfbot-v13');
-const client = new Discord.Client({
-  readyStatus: false,
-  checkUpdate: false
-});
-
+const client = new Discord.Client({ readyStatus: false, checkUpdate: false });
 const keepAlive = require('./server.js');
 keepAlive();
+
+let startTime = Date.now();
 
 client.on('ready', async () => {
     console.clear();
     console.log(`${client.user.tag} - made by: @nozcy.`);
 
-    const r = new Discord.RichPresence()
-        .setApplicationId('1107744228773220473')
-        .setType('PLAYING')
-        .setURL('https://zcyy-website.vercel.app')
-        .setState('Click The Button below to join my server')
-        .setName('Nothing')
-        .setDetails('pls join My Community Server')
-        .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1233503643500675252/1256997956637425674/c7ce50ce-fbbe-4589-a0e4-0c47a8d982ce.png?ex=6682ce12&is=66817c92&hm=76379a7f03b06623c8f9e7cf60f3f42d38a273c3bfffc7d22b917a7f849b60b8&')
-        .addButton('click here', 'https://discord.com/invite/tqnDqzmbxE')
+    setInterval(() => {
+        let currentTime = Date.now();
+        let elapsedTime = Math.floor((currentTime - startTime) / 1000);
+        let hours = Math.floor(elapsedTime / 3600);
+        let minutes = Math.floor((elapsedTime % 3600) / 60);
+        let seconds = elapsedTime % 60;
 
-    client.user.setActivity(r);
-    client.user.setPresence({ status: 'dnd' });
+        let formattedTime;
+        if (hours > 0) {
+            formattedTime = `${hours} hour${hours > 1 ? 's' : ''}, ${minutes} minute${minutes > 1 ? 's' : ''}`;
+        } else if (minutes > 0) {
+            formattedTime = `${minutes} minute${minutes > 1 ? 's' : ''}, ${seconds} second${seconds > 1 ? 's' : ''}`;
+        } else {
+            formattedTime = `${seconds} second${seconds > 1 ? 's' : ''}`;
+        }
+
+        const r = new Discord.RichPresence()
+            .setApplicationId('1107744228773220473')
+            .setType('COMPETING')
+            .setState('To win your heart')
+            .setName('Tournament')
+            .setDetails(`Elapsed: ${formattedTime}`)
+            .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1233503643500675252/1256997956637425674/c7ce50ce-fbbe-4589-a0e4-0c47a8d982ce.png')
+            .addButton('My Website', 'https://file-hosting.onrender.com');
+
+        client.user.setActivity(r);
+        client.user.setPresence({ status: 'dnd' });
+    }, 10000);
 });
 
 client.login(process.env.TOKEN);
