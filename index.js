e13c16b80cc070dc59e4c9aedcc1cba67d5f9168
Zcy. <157526994+4ozCy@users.noreply.github.com>
2024-09-17 23:42:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ client.on('ready', async () => {
             .setState('Host by a non-existing person')
             .setName('Tournament')
             .setDetails('To win your heart')
-            .setStartTimestamp(Date.now)()
             .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1233503643500675252/1256997956637425674/c7ce50ce-fbbe-4589-a0e4-0c47a8d982ce.png?ex=66ea4bd2&is=66e8fa52&hm=6c4243d7cb46548b8b712fbfedc5df421bfaa9e57021c33a177e195a2ba9dca5&')
+            .setStartTimestamp(Date.now)();
 
         client.user.setActivity(r);
         client.user.setPresence({ status: 'dnd' });
