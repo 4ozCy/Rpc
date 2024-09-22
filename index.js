568a067a49f246abff1ce2a15e106807f1d34d2e
Zcy. <157526994+4ozCy@users.noreply.github.com>
2024-09-23 02:18:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,18 +7,29 @@ client.on('ready', async () => {
     console.clear();
     console.log(`${client.user.tag} - rpc started.`);
 
-        const r = new Discord.RichPresence()
-            .setApplicationId('1107744228773220473')
-            .setType('COMPETING')
-            .setState('Host by a non-existing person')
-            .setName('Tournament')
-            .setDetails('To win your heart')
-            .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1233503643500675252/1256997956637425674/c7ce50ce-fbbe-4589-a0e4-0c47a8d982ce.png?ex=66ea4bd2&is=66e8fa52&hm=6c4243d7cb46548b8b712fbfedc5df421bfaa9e57021c33a177e195a2ba9dca5&')
-            .setStartTimestamp(Date.now())
-            .addButton('My Website', 'https://filebox-op.onrender.com/');
+    const r = new Discord.RichPresence()
+        .setApplicationId('1107744228773220473')
+        .setType('COMPETING')
+        .setState('Host by a non-existing person')
+        .setName('Tournament')
+        .setDetails('To win your heart')
+        .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1233503643500675252/1256997956637425674/c7ce50ce-fbbe-4589-a0e4-0c47a8d982ce.png?ex=66ea4bd2&is=66e8fa52&hm=6c4243d7cb46548b8b712fbfedc5df421bfaa9e57021c33a177e195a2ba9dca5&')
+        .setStartTimestamp(Date.now())
+        .addButton('My Website', 'https://filebox-op.onrender.com/');
 
-        client.user.setActivity(r);
-        client.user.setPresence({ status: 'dnd' });
+    client.user.setActivity(r);
+    client.user.setPresence({ status: 'dnd' });
+
+    const channelId = '1282119874675605575';
+    const channel = client.channels.cache.get(channelId);
+
+    if (channel) {
+        setInterval(() => {
+            channel.startTyping();
+        }, 5000);
+    } else {
+        console.error('Channel not found.');
+    }
 });
 
 client.login(process.env.TOKEN);
