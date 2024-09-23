bcaf4fad9265f9907ff9d4450ed60b9955c1f47c
Zcy. <157526994+4ozCy@users.noreply.github.com>
2024-09-23 16:45:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.on('ready', async () => {
         .setApplicationId('1107744228773220473')
         .setType('COMPETING')
         .setState('Host by a non-existing person')
-        .setName('Bot Development Tournament')
+        .setName('Some not legal activity')
         .setDetails('To win your heart')
         .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1233503643500675252/1256997956637425674/c7ce50ce-fbbe-4589-a0e4-0c47a8d982ce.png?ex=66ea4bd2&is=66e8fa52&hm=6c4243d7cb46548b8b712fbfedc5df421bfaa9e57021c33a177e195a2ba9dca5&')
         .setStartTimestamp(Date.now())
