14cbc1608190b93ffa6559cf5344706402e552ea
Zcy. <157526994+4ozCy@users.noreply.github.com>
2024-10-14 13:38:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ client.on('ready', async () => {
         .setState('Host by a non-existing person')
         .setName('Tournament')
         .setDetails('To win your heart')
-        .setAssetsLargeImage('http://files-box.vercel.app/file/994db.png')
+        .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1233503643500675252/1295274070564143179/c7ce50ce-fbbe-4589-a0e4-0c47a8d982ce.png?ex=670e0d7b&is=670cbbfb&hm=fafbf458627f68c0d61bcf6b9650ac33ae0d7b49d26863a34fdc1356c303e0e2&')
         .setStartTimestamp(Date.now())
         .addButton('My Website', 'https://files-box.vercel.app/');
 
